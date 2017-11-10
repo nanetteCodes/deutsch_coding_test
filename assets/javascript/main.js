8696bd0d9306846436ef1ba0e4bd6f5e68ea3be4
Nanette <njulius89@gmail.com>
2017-11-10 13:11:23 -0800

    initial commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+  new WOW().init();
+
+  $("#racer").delay(1000).animate({
+    left: "+=-30"
+  }, 300);
+
+  $('#one-countdown')
+    .prop('number', 10)
+    .delay(600)
+    .animateNumber(
+      {
+        number: 1
+      },
+    1500
+  );
+
+  $('#two-countdown')
+    .prop('number', 10)
+    .delay(900)
+    .animateNumber(
+      {
+        number: 2
+      },
+    1500
+  );
+
+  $('#three-countdown')
+    .prop('number', 10)
+    .delay(1200)
+    .animateNumber(
+      {
+        number: 3
+      },
+    1500
+  );
+
+  $('#four-countdown')
+    .prop('number', 10)
+    .delay(1500)
+    .animateNumber(
+      {
+        number: 4
+      },
+    1500
+  );
+});
